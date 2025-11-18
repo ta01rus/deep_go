f09e12b90a169ce87f14d0682e0439bc1ae14ab1
acus <ta01rus@gmail.com>
2025-11-18 19:25:45 +0300

    save

--- Task-09/homework_test.go
+++ Task-09/homework_test.go
@@ -27,10 +27,65 @@ type Car struct {
 	Owners       []string          `properties:"owners,omitempty"`
 }
 
+func options(tag string) (name string, omitempty bool) {
+	options := strings.Split(tag, ",")
+	l := len(options)
+
+	if l == 0 {
+		return "", false
+	}
+
+	o1, o2 := options[0], ""
+	if l > 1 {
+		o2 = options[1]
+	}
+
+	switch {
+	case o1 == "omitempty":
+		return o2, true
+	case o2 == "omitempty":
+		return o1, true
+	}
+
+	return o1, false
+}
+
+func SerializeStruct(obj any) string {
+	objValueType := reflect.ValueOf(obj)
+
+	if objValueType.Kind() != reflect.Struct {
+		panic("obj is not struct")
+	}
+
+	objType := reflect.TypeOf(obj)
+	parts := make([]string, 0, objType.NumField())
+	for i := 0; i < objType.NumField(); i++ {
+		fieldValue, fieldType := objValueType.Field(i), objType.Field(i)
+
+		key := fieldType.Name
+		value := Serialize(fieldValue.Interface())
+
+		if s, ok := fieldType.Tag.Lookup("properties"); ok {
+			name, omitempty := options(s)
+
+			switch {
+			case value == "" && omitempty:
+				continue
+			}
+			if name != "" {
+				key = name
+			}
+		}
+		parts = append(parts, key+"="+value)
+	}
+	return strings.Join(parts, "\n")
+
+}
+
 func Serialize(obj any) string {
 
 	objValueType := reflect.ValueOf(obj)
-	objType := reflect.TypeOf(obj)
+	// objType := reflect.TypeOf(obj)
 
 	switch objValueType.Kind() {
 	case reflect.Int64, reflect.Int, reflect.Int32, reflect.Int8:
@@ -65,32 +120,7 @@ func Serialize(obj any) string {
 		case time.Time:
 			return t.Format("2006-01-02 15:04:05")
 		default:
-			parts := make([]string, 0, objType.NumField())
-			for i := 0; i < objType.NumField(); i++ {
-				fieldValue, fieldType := objValueType.Field(i), objType.Field(i)
-
-				key := fieldType.Name
-				value := Serialize(fieldValue.Interface())
-
-				if tags, ok := fieldType.Tag.Lookup("properties"); ok {
-					options := strings.Split(tags, ",")
-
-					if len(options) > 0 && options[0] != "" {
-						key = options[0]
-					}
-					switch {
-					case value == "" && len(options) < 1:
-						panic(fmt.Errorf("%s is empty ", fieldType.Name))
-					case value == "" && len(options) > 1 && options[1] != "omitempty":
-						panic(fmt.Errorf("%s is empty ", fieldType.Name))
-					case value == "" && len(options) > 1 && options[1] == "omitempty":
-						continue
-					}
-
-				}
-				parts = append(parts, key+"="+value)
-			}
-			return strings.Join(parts, "\n")
+			return SerializeStruct(obj)
 		}
 
 	case reflect.String:
@@ -187,6 +217,71 @@ func TestCarSerialization(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			result := Serialize(test.car)
 			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func Test_options(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		tag   string
+		want  string
+		want2 bool
+	}{
+		{
+			"test 1",
+			"omitempty,power",
+			"power",
+			true,
+		},
+		{
+			"test 2",
+			"power,omitempty",
+			"power",
+			true,
+		},
+		{
+			"test 3",
+			",omitempty",
+			"",
+			true,
+		},
+		{
+			"test 4",
+			"omitempty",
+			"",
+			true,
+		},
+		{
+			"test 5",
+			"power",
+			"power",
+			false,
+		},
+		{
+			"test 6",
+			"power",
+			"power",
+			false,
+		},
+		{
+			"test 7",
+			"omitempty",
+			"",
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got2 := options(tt.tag)
+			// TODO: update the condition below to compare got with tt.want.
+			if got != tt.want {
+				t.Errorf("options() = %v, want %v", got, tt.want)
+			}
+			if got2 != tt.want2 {
+				t.Errorf("options() = %v, want %v", got2, tt.want2)
+			}
 		})
 	}
 }
